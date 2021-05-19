eca6d17d18d4f30d3f7ca5dbdd36db1c3ed5243e
Kajutin <s037045@ad.viko.lt>
2021-05-19 20:31:27 +0300

    Initial commit

---
+++ Sifravimas V4.1/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace Sifravimas_V4._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PasswordsListView = new System.Windows.Forms.ListView();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.URLTextBox = new System.Windows.Forms.TextBox();
+            this.CommentTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.ChangePasswordTextBox = new System.Windows.Forms.TextBox();
+            this.ChangePasswordButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SearchResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PasswordsListView
+            // 
+            this.PasswordsListView.HideSelection = false;
+            this.PasswordsListView.Location = new System.Drawing.Point(12, 11);
+            this.PasswordsListView.Name = "PasswordsListView";
+            this.PasswordsListView.Size = new System.Drawing.Size(522, 227);
+            this.PasswordsListView.TabIndex = 0;
+            this.PasswordsListView.UseCompatibleStateImageBehavior = false;
+            this.PasswordsListView.View = System.Windows.Forms.View.List;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(608, 30);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(185, 23);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(608, 59);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(185, 23);
+            this.PasswordTextBox.TabIndex = 2;
+            // 
+            // URLTextBox
+            // 
+            this.URLTextBox.Location = new System.Drawing.Point(608, 88);
+            this.URLTextBox.Name = "URLTextBox";
+            this.URLTextBox.Size = new System.Drawing.Size(185, 23);
+            this.URLTextBox.TabIndex = 3;
+            // 
+            // CommentTextBox
+            // 
+            this.CommentTextBox.Location = new System.Drawing.Point(608, 117);
+            this.CommentTextBox.Name = "CommentTextBox";
+            this.CommentTextBox.Size = new System.Drawing.Size(185, 23);
+            this.CommentTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(568, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(550, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "URL/App";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(546, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Comment";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(728, 146);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(65, 23);
+            this.SaveButton.TabIndex = 9;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Location = new System.Drawing.Point(12, 244);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(158, 23);
+            this.SearchTextBox.TabIndex = 10;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(176, 244);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(65, 23);
+            this.SearchButton.TabIndex = 11;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(324, 244);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(210, 23);
+            this.CopyButton.TabIndex = 12;
+            this.CopyButton.Text = "Copy selected password to clipboard";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // ChangePasswordTextBox
+            // 
+            this.ChangePasswordTextBox.Location = new System.Drawing.Point(12, 324);
+            this.ChangePasswordTextBox.Name = "ChangePasswordTextBox";
+            this.ChangePasswordTextBox.Size = new System.Drawing.Size(229, 23);
+            this.ChangePasswordTextBox.TabIndex = 13;
+            // 
+            // ChangePasswordButton
+            // 
+            this.ChangePasswordButton.Location = new System.Drawing.Point(78, 353);
+            this.ChangePasswordButton.Name = "ChangePasswordButton";
+            this.ChangePasswordButton.Size = new System.Drawing.Size(163, 23);
+            this.ChangePasswordButton.TabIndex = 14;
+            this.ChangePasswordButton.Text = "Change found password";
+            this.ChangePasswordButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(546, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 15);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Create a new password:";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(78, 382);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(163, 23);
+            this.DeleteButton.TabIndex = 16;
+            this.DeleteButton.Text = "Delete found password";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 306);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(142, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Enter your new password:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(614, -109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 23);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 270);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Search result:";
+            // 
+            // SearchResultLabel
+            // 
+            this.SearchResultLabel.AutoSize = true;
+            this.SearchResultLabel.Location = new System.Drawing.Point(12, 285);
+            this.SearchResultLabel.Name = "SearchResultLabel";
+            this.SearchResultLabel.Size = new System.Drawing.Size(92, 15);
+            this.SearchResultLabel.TabIndex = 19;
+            this.SearchResultLabel.Text = "[results go here]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 411);
+            this.Controls.Add(this.SearchResultLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ChangePasswordButton);
+            this.Controls.Add(this.ChangePasswordTextBox);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.SearchTextBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CommentTextBox);
+            this.Controls.Add(this.URLTextBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.PasswordsListView);
+            this.Name = "Form1";
+            this.Text = "Password manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView PasswordsListView;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox URLTextBox;
+        private System.Windows.Forms.TextBox CommentTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.TextBox ChangePasswordTextBox;
+        private System.Windows.Forms.Button ChangePasswordButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label SearchResultLabel;
+    }
+}
+
---
+++ Sifravimas V4.1/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Sifravimas_V4._1
+{
+    public partial class Form1 : Form
+    {
+        string path = "C:\\Users\\kajus\\Desktop\\testSaugumas\\";
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists(path + "passwords.txt.aes"))
+            {
+                FileDecrypt(path + "passwords.txt.aes", "test");
+                File.Delete(path + "passwords.txt.aes");    
+            }
+            else
+            {
+                File.Create(path + "passwords.txt").Close();
+            }
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            FileEncrypt(path + "passwords.txt", "test");
+            File.Delete(path + "passwords.txt");
+            base.OnFormClosing(e);
+        }
+        private void FileEncrypt(string inputFile, string password)
+        {
+            //http://stackoverflow.com/questions/27645527/aes-encryption-on-large-files
+
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            using FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            AES.Mode = CipherMode.CBC;
+
+            // write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            using CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            using FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                    cs.Write(buffer, 0, read);
+                }
+                // Close up
+                fsIn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            //finally
+            //{
+            //    cs.Close();
+            //    fsCrypt.Close();
+            //    using (var md5 = MD5.Create())
+            //    {
+            //        using (var stream = File.OpenRead(inputFile + ".aes"))
+            //        {
+            //            hashDictionary.Add(Convert.ToBase64String(md5.ComputeHash(stream)), inputFile + ".aes");
+            //        }
+            //    }
+            //}
+        }
+        private void FileDecrypt(string inputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CBC;
+            string outputFile = inputFile.Substring(0, inputFile.Length - 4);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fille the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        public void FillListViewFromFile()
+        {
+            PasswordsListView.Clear();
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            using System.IO.StreamReader file =
+                new System.IO.StreamReader(path + "passwords.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] tempList = line.Split("<>");
+                string name = tempList[0];
+                string password = tempList[1];
+                string url = tempList[2];
+                string comment = tempList[3];
+                string passwordOutput;
+                byte[] KeyAES = Encoding.UTF8.GetBytes("testtesttesttest");
+                passwordOutput = DesifruojamBaitusITeksta_ECB(password, KeyAES);
+                PasswordsListView.Items.Add(name + " // " + passwordOutput + " // " + url + " // " + comment);
+                counter++;
+            }
+        }
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string Name = NameTextBox.Text;
+            string Password = PasswordTextBox.Text;
+            string URL = URLTextBox.Text;
+            string Comment = CommentTextBox.Text;
+            string PasswordEncrypted;
+
+            byte[] KeyAES = Encoding.UTF8.GetBytes("testtesttesttest");
+            string encrypted = SifruojamTekstaIBaitus_ECB(Password, KeyAES);
+            string Final = Name + "<>" + encrypted + "<>" + URL + "<>" + Comment + "\n";
+            File.AppendAllText(path + "passwords.txt", Final);          
+            FillListViewFromFile();
+            NameTextBox.Text = "";
+            PasswordTextBox.Text = "";
+            URLTextBox.Text = "";
+            CommentTextBox.Text = "";
+        }
+        private string SifruojamTekstaIBaitus_ECB(string text, byte[] key)
+        {
+            // Nesifruotas tekstaspaverciamas i baitus
+            byte[] tekstas = Encoding.UTF8.GetBytes(text);
+            RijndaelManaged aes = new RijndaelManaged();
+            // Modas ecb
+            aes.Mode = CipherMode.ECB;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.KeySize = 128;
+
+            // Sukuria sifratoriu
+            using (ICryptoTransform sifratorius = aes.CreateEncryptor(key, null))
+            {
+                // Sifruojam teksta
+                byte[] sifruotasTekstas = sifratorius.TransformFinalBlock(tekstas, 0, tekstas.Length);
+                // Nutraukiam darba
+                sifratorius.Dispose();
+                // Grazinam sifruota teksta string formatu
+                return Convert.ToBase64String(sifruotasTekstas);
+            }
+        }
+        private string DesifruojamBaitusITeksta_ECB(string text, byte[] key)
+        {
+            // Konvertuoja teksta i baitus
+            byte[] sifruotasTekstas = Convert.FromBase64String(text);
+            RijndaelManaged aes = new RijndaelManaged();
+            // Nustatom rakto dydi
+            aes.KeySize = 128;
+            aes.Padding = PaddingMode.PKCS7;
+            // Nuastatom moda i ecb
+            aes.Mode = CipherMode.ECB;
+
+            // Sukuria desifratoriu
+            using (ICryptoTransform desifratorius = aes.CreateDecryptor(key, null))
+            {
+                byte[] desifruotasTekstas = desifratorius.TransformFinalBlock(sifruotasTekstas, 0, sifruotasTekstas.Length);
+                // Nutraukia desifravimo darba
+                desifratorius.Dispose();
+                // Grazinam Desifruota teksta string formatu
+                return Encoding.UTF8.GetString(desifruotasTekstas);
+            }
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            string fullString = PasswordsListView.SelectedItems[0].Text;
+            string[] stringList = fullString.Split(" // ");
+            string passwordSnippet = stringList[1];
+            Clipboard.SetText(passwordSnippet);
+
+        }
+    }
+}
+
